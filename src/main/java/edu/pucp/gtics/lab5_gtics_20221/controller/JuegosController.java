b2856d09b2946d0233b2893429f1686dde847e15
piero <fernandezpiero11@gmail.com>
2022-05-03 20:12:06 -0500

    xd

--- src/main/java/edu/pucp/gtics/lab5_gtics_20221/controller/JuegosController.java
+++ src/main/java/edu/pucp/gtics/lab5_gtics_20221/controller/JuegosController.java
@@ -12,7 +12,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
