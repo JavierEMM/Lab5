2d08fa1668b13d1bae6ff87ba567dd46c8d09528
Leonardo Ayala <leonardo.ayalao@pucp.edu.pe>
2022-05-03 23:13:45 -0500

    juegosxusuario

---
+++ src/main/java/edu/pucp/gtics/lab5_gtics_20221/repository/JuegosxUsuarioRepository.java
@@ -0,0 +1,7 @@
+package edu.pucp.gtics.lab5_gtics_20221.repository;
+
+import edu.pucp.gtics.lab5_gtics_20221.entity.JuegosxUsuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JuegosxUsuarioRepository extends JpaRepository<JuegosxUsuario,Integer> {
+}
